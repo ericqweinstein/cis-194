904ed6ccfd9cc5183fe5bd53f92910152b989c06
Eric Weinstein <eric.q.weinstein@gmail.com>
2015-12-23 17:45:58 -0500

    More idiomatic naming

--- one/Hanoi.hs
+++ one/Hanoi.hs
@@ -33,7 +33,7 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi 0 a b c = []
+hanoi 0 _ _ _ = []
 hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
 -- Read as:
 --              ^ "move n - 1 discs from a to c using b as temporary storage" (Rule #1, above)
